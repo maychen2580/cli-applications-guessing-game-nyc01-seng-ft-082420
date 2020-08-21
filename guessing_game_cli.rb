aa9a9896928705e5ae28eccb5f44fc04345cab7f
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-21 03:06:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   random = rand(1..6)
   puts "Please guess a number between 1 to 6:"
   user_input = gets.chomp
-  if user_input = random.to_s
+  if user_input == random.to_s
     puts "You guessed the correct number!"
   end
 end
