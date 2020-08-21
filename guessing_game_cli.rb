30fbcb35a956f485f2b9114f16a0dcf2f71179ce
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-21 03:10:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@ def run_guessing_game
   random = rand(1..6)
   puts "Please guess a number between 1 to 6:"
   user_input = gets.chomp
-  if user_input = random.to_s
+  if user_input == random.to_s
     puts "You guessed the correct number!"
     elsif
-    user_input == "exit"
+    user_input.downcase == "exit"
     puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{random}."
